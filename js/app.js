30261bef8deaf17ab44273749781b2c9c1fdcb01
Muhammad Junaid <juni4work@gmail.com>
2018-05-22 08:22:28 +0500

    javascript changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+
+const searchBtn = document.querySelector('.search');
+const searchBox = document.querySelector('.searchBox');
+const closeBtn = document.querySelector('.closeSearch');
+const vanishBtn = document.querySelector('.vanish');
+const products = document.querySelector('.products')
+
+searchBtn.addEventListener('click',openSearch);
+closeBtn.addEventListener('click',closeSearch);
+
+
+
+function openSearch(event){
+	event.preventDefault;
+
+	searchBox.className += ' open';
+
+}
+
+
+function closeSearch(event){
+	event.preventDefault;
+
+	searchBox.className = 'searchBox';
+
+}
+
+
 new WOW().init();
