0a8eb041f217a285eb577869ed734ef30313e7f9
Muhammad Junaid <juni4work@gmail.com>
2018-05-23 15:59:59 +0500

    cart added & style improved

--- js/app.js
+++ js/app.js
@@ -3,12 +3,24 @@ const searchBtn = document.querySelector('.search');
 const searchBox = document.querySelector('.searchBox');
 const closeBtn = document.querySelector('.closeSearch');
 const vanishBtn = document.querySelector('.vanish');
-const products = document.querySelector('.products')
+const products = document.querySelector('.products');
+const basket = document.querySelector('.basket');
+const mainSec = document.querySelector('#main');
 
 searchBtn.addEventListener('click',openSearch);
 closeBtn.addEventListener('click',closeSearch);
+basket.addEventListener('click', toggleBasket);
 
+function toggleBasket(){
 
+	if(mainSec.classList.contains('open')){
+		mainSec.className = 'home-page'
+	}	else {
+		mainSec.className += ' open';
+	}
+
+	
+}
 
 function openSearch(event){
 	event.preventDefault;
