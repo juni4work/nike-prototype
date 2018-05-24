1ad62f6b9e9671087a9812c1d9199b06a3b18d52
Muhammad Junaid <juni4work@gmail.com>
2018-05-24 18:10:49 +0500

    bg transitions added

--- js/app.js
+++ js/app.js
@@ -5,22 +5,23 @@ const closeBtn = document.querySelector('.closeSearch');
 const vanishBtn = document.querySelector('.vanish');
 const products = document.querySelector('.products');
 const basket = document.querySelector('.basket');
+const closeBasket = document.querySelector('.closeCart');
+const cart	= document.querySelector('.cart');
 const mainSec = document.querySelector('#main');
 
 searchBtn.addEventListener('click',openSearch);
 closeBtn.addEventListener('click',closeSearch);
-basket.addEventListener('click', toggleBasket);
+basket.addEventListener('click', function(event){
+		event.preventDefault;
+		revealAnim(cart);
+		cart.className += ' open';
+});
+closeBasket.addEventListener('click', function(event){
+		event.preventDefault;
+		cart.className = 'cart';
 
-function toggleBasket(){
+});
 
-	if(mainSec.classList.contains('open')){
-		mainSec.className = 'home-page'
-	}	else {
-		mainSec.className += ' open';
-	}
-
-	
-}
 
 function openSearch(event){
 	event.preventDefault;
@@ -34,6 +35,36 @@ function closeSearch(event){
 	event.preventDefault;
 
 	searchBox.className = 'searchBox';
+
+}
+
+const productItems = document.querySelectorAll('.products > ul > li');
+
+for(let i=0 ; i< productItems.length ; i++){
+
+	productItems[i].addEventListener("mouseenter",function(event){
+		let bg = event.target.getAttribute('data-bg');
+		mainSec.className = bg;
+	});
+
+	productItems[i].addEventListener("mouseleave",function(event){
+		mainSec.className = "grad-main";
+	})
+}
+
+
+function revealAnim (element){
+	let childs = element.children;
+	console.log(childs.length);
+
+
+
+		for(let i=0; i<childs.length ; i++){
+
+			childs[i].className += ' wow fadeInUp';		
+			
+		}
+
 
 }
 
